ff2f0d8e2ebe4885d2d8cea599d4b88d514a751c
Albert Bori <github@albertbori.com>
2016-12-27 17:55:32 -0700

    Added slash command support

--- GuildUI.lua
+++ GuildUI.lua
@@ -1,4 +1,9 @@
 -- Main Functions/Code
 local _, GuildUI=...
 
+SLASH_GUILDUI1 = '/guildui';
+function SlashCmdList.GUILDUI(msg, editbox)
+    print("TODO: Do a thing.");
+end
+
 -- TODO: UI Code. Call the stuff in here from Events.lua
